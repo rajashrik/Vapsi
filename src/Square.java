f4c657399ceb9ca95f892f9faf0a2b96b2dedb94
Rajashri <jadhavrajashri49@gmail.com>
2017-08-02 11:06:31 +0530

    initial commit to calculate area of shapes

---
+++ src/Square.java
@@ -0,0 +1,15 @@
+/**
+ * Created by rajashrk on 8/2/17.
+ */
+public class Square {
+
+    private double length;
+
+    public Square(double length) {
+        this.length = length;
+    }
+
+    public double area() {
+        return length * length;
+    }
+}
